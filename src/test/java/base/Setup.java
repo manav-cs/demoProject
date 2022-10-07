153a5fc314022a071d6a08423dc8a72cd75216c7
Manav Chokshi <manav@ip-192-168-0-104.ec2.internal>
2022-10-07 14:48:31 +0530

    123

--- src/test/java/base/Setup.java
+++ src/test/java/base/Setup.java
@@ -21,25 +21,26 @@ public class Setup {
 
     public WebDriver initDriver(String driverType) {
         if (driverType.equalsIgnoreCase("ch")) {
-            WebDriverManager.chromedriver().setup();
-            ChromeOptions options = new ChromeOptions();
+           // WebDriverManager.chromedriver().setup();
+  //          ChromeOptions options = new ChromeOptions();
             //options.setHeadless(true);
-            options.addArguments("--incognito");
-            options.addArguments("--test-type");
-            options.addArguments("--disable-popup-blocking");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--disable-crash-reporter");
-            options.addArguments("--disable-extensions");
-            options.addArguments("--disable-in-process-stack-traces");
-            options.addArguments("--disable-logging");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--log-level=3");
-            options.addArguments("--output=/dev/null");
-           // System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
-            System.setProperty("webdriver.chrome.silentOutput", "true");
-            caps.setCapability(ChromeOptions.CAPABILITY, options);
-            driver = new ChromeDriver(options);
+//            options.setBinary("/Users/manav/Downloads/chromedriver");
+//            options.addArguments("--incognito");
+//            options.addArguments("--test-type");
+//            options.addArguments("--disable-popup-blocking");
+//            options.addArguments("--no-sandbox");
+//            options.addArguments("--disable-gpu");
+//            options.addArguments("--disable-crash-reporter");
+//            options.addArguments("--disable-extensions");
+//            options.addArguments("--disable-in-process-stack-traces");
+//            options.addArguments("--disable-logging");
+//            options.addArguments("--disable-dev-shm-usage");
+//            options.addArguments("--log-level=3");
+//            options.addArguments("--output=/dev/null");
+            System.setProperty("webdriver.chrome.driver", "/Users/manav/Downloads/chromedriver");
+         //  System.setProperty("webdriver.chrome.silentOutput", "true");
+          //  caps.setCapability(ChromeOptions.CAPABILITY, options);
+            driver = new ChromeDriver();
         } else if (driverType.equalsIgnoreCase("ff")) {
             WebDriverManager.firefoxdriver().setup();
             FirefoxOptions options = new FirefoxOptions();
